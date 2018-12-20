46dfd0926b351c92a179f0a8aa043110082c6210
german <cisnerosgd@gmail.com>
2018-12-20 20:33:40 -0300

    add removed cascade

---
+++ src/main/java/com/cash/examen/models/Loan.java
@@ -0,0 +1,49 @@
+package com.cash.examen.models;
+
+import java.math.BigDecimal;
+
+import javax.persistence.Column;
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+import javax.persistence.Table;
+
+@Entity
+@Table(name = "loans")
+public class Loan {
+	@Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+ 
+    @Column(nullable = false)
+	private BigDecimal total;
+    
+    @Column(nullable = false)
+    private int user_id;
+
+	public Long getId() {
+		return id;
+	}
+
+	public void setId(Long id) {
+		this.id = id;
+	}
+
+	public BigDecimal getTotal() {
+		return total;
+	}
+
+	public void setTotal(BigDecimal total) {
+		this.total = total;
+	}
+
+	public int getUser_id() {
+		return user_id;
+	}
+
+	public void setUser_id(int user_id) {
+		this.user_id = user_id;
+	}
+    
+}
--- src/main/java/com/cash/examen/models/User.java
+++ src/main/java/com/cash/examen/models/User.java
@@ -1,10 +1,16 @@
 package com.cash.examen.models;
 
+import java.util.ArrayList;
+import java.util.List;
+
+import javax.persistence.CascadeType;
 import javax.persistence.Column;
 import javax.persistence.Entity;
 import javax.persistence.GeneratedValue;
 import javax.persistence.GenerationType;
 import javax.persistence.Id;
+import javax.persistence.JoinColumn;
+import javax.persistence.OneToMany;
 import javax.persistence.Table;
 
 @Entity
@@ -17,6 +23,11 @@ public class User {
     @Column(nullable = false)
     private String first_name;
 
+    @OneToMany(mappedBy = "user_id", cascade = CascadeType.ALL,orphanRemoval = true)
+   // @JoinColumn(name = "user_id")
+    private List<Loan> loans = new ArrayList<>();
+    
+	
 	public Long getId() {
 		return id;
 	}
@@ -32,6 +43,15 @@ public class User {
 	public void setFirst_name(String first_name) {
 		this.first_name = first_name;
 	}
+	
+	public List<Loan> getLoans() {
+		return loans;
+	}
+
+	public void setLoans(List<Loan> loans) {
+		this.loans = loans;
+	}
+
     
     
 
